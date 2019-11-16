c73375330a2b095595852c03a9e8eba8e545a614
gmachielsen <g.machielsen@gmail.com>
2019-11-16 17:33:01 +0100

    refined register/login

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('accounts/', include("django.contrib.auth.urls")),
     path('register', views.register, name= 'register'),
-    path('login', views.login, name='login'),
+    # path('login', views.login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,30 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
-from django.contrib.auth import login, logout, authenticate
+# from django.contrib.auth.models import User
+# from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import RegisterForm
+from django.contrib.auth import login, authenticate
+# from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 
-def register(response):
-    if response.method == "POST":
-        form = UserCreationForm(response.POST)
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-        return redirect('login')
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            raw_password1 = form.cleaned_data.get('password1')
+            raw_password2 = form.cleaned_data.get('password2')
+
+            user = authenticate(username=username, password=raw_password1)
+            login(request, user)
+
+        return redirect('post_list')
     else:
-        form = UserCreationForm()
-    return render(response, "registration/register.html", {"form": form})
+        form = RegisterForm()
+
+    return render(request, "registration/register.html", {"form": form})
 
 # def login(request):
 #     if request.method == 'POST':
