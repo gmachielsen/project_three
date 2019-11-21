9fdeb8071af5a3946e04f44b34b644ccb7f8577e
gmachielsen <g.machielsen@gmail.com>
2019-11-21 13:25:42 +0100

    initial commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'),),
-    # path('accounts/', include("django.contrib.auth.urls")),
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'), name='custom_password'),
+    path('accounts/', include("django.contrib.auth.urls")),
     path('register', views.register, name= 'register'),
 ]
