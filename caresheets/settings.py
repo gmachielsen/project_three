a54f0ba79aa06103e45c29996fd37f12c23536ab
gmachielsen <g.machielsen@gmail.com>
2019-12-05 11:59:22 +0100

    initial commit

--- caresheets/settings.py
+++ caresheets/settings.py
@@ -133,9 +133,7 @@ LOGOUT_REDIRECT_URL = "/"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT =  os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = (
- os.path.join(BASE_DIR, 'static'),
-)
+
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static')
 # ]
