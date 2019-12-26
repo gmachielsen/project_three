a20ac7dc0f13784d0a71c6c5220de3dfc05de65d
gmachielsen <g.machielsen@gmail.com>
2019-12-26 22:34:01 +0100

    initial commit styled postdetail and indexpage

--- caresheets/settings.py
+++ caresheets/settings.py
@@ -144,7 +144,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static/'),
+# )
 
 
 # MEDIAFILES_DIRS = (
