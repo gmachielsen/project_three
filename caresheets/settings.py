e3f0b6af519c0cd3212d002b8d5b9ae96b7f2bb0
gmachielsen <g.machielsen@gmail.com>
2020-01-01 14:02:21 +0100

    initial commit

--- caresheets/settings.py
+++ caresheets/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$_9ja&6=vwx3cequ4tyh=62y9nqxk6==t&a4eikgq6#zwz*n)e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codeinstitutecaresheets.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 # 127.0.0.1
 # codeinstitutecaresheets.herokuapp.com
 # Application definition
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['codeinstitutecaresheets.herokuapp.com']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -41,6 +42,10 @@ INSTALLED_APPS = [
     'django_forms_bootstrap',
     'accounts',
     'posts',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.facebook',
 ]
 
 MIDDLEWARE = [
@@ -167,3 +172,10 @@ LOGOUT_REDIRECT_URL = "/"
 #     '/project_three/caresheets/static',
 # ]
 # emaillogin_project/settings.py
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID=1
