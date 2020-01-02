f3a95c03f108492474c5c0be3a2942233ff9a699
gmachielsen <g.machielsen@gmail.com>
2020-01-02 13:08:40 +0100

    styling

--- caresheets/settings.py
+++ caresheets/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$_9ja&6=vwx3cequ4tyh=62y9nqxk6==t&a4eikgq6#zwz*n)e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['codeinstitutecaresheets.herokuapp.com']
 # 127.0.0.1
 # codeinstitutecaresheets.herokuapp.com
 # Application definition
